8545efad9612997078b6d72aafbfbd0196358509
ruimendes <rui.mendes@busbud.com>
2025-11-08 11:10:07 +0000

    Create Texfields for authentication

---
+++ core/designsystem/src/commonMain/kotlin/com/ruimendes/core/designsystem/components/textfields/AppTextField.kt
@@ -0,0 +1,160 @@
+package com.ruimendes.core.designsystem.components.textfields
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.ruimendes.core.designsystem.theme.AppTheme
+import com.ruimendes.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AppTextField(
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    title: String? = null,
+    supportingText: String? = null,
+    isError: Boolean = false,
+    singleLine: Boolean = false,
+    enabled: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    onFocusChanged: (Boolean) -> Unit = {}
+) {
+    AppTextFieldLayout(
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged,
+        modifier = modifier
+    ) { styleModifier, interactionSource ->
+        BasicTextField(
+            state = state,
+            enabled = enabled,
+            lineLimits = if (singleLine) {
+                TextFieldLineLimits.SingleLine
+            } else {
+                TextFieldLineLimits.Default
+            },
+            textStyle = MaterialTheme.typography.bodyMedium.copy(
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.onSurface
+                } else {
+                    MaterialTheme.colorScheme.extended.textPlaceholder
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
+            interactionSource = interactionSource,
+            decorator = { innerBox ->
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    if (state.text.isEmpty() && placeholder != null) {
+                        Text(
+                            text = placeholder,
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.extended.textPlaceholder
+                        )
+                    }
+                    innerBox()
+                }
+            },
+            modifier = styleModifier
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun AppTextFieldEmptyPreview() {
+    AppTheme {
+        AppTextField(
+            state = rememberTextFieldState(),
+            modifier = Modifier.width(300.dp),
+            placeholder = "eder.lopes@portugal.pt",
+            title = "Email",
+            supportingText = "Please enter your email."
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun AppTextFieldFilledPreview() {
+    AppTheme {
+        AppTextField(
+            state = rememberTextFieldState("eder.lopes@portugal.pt"),
+            modifier = Modifier.width(300.dp),
+            placeholder = "eder.lopes@portugal.pt",
+            title = "Email",
+            supportingText = "Please enter your email."
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun AppTextFieldDisabledPreview() {
+    AppTheme {
+        AppTextField(
+            state = rememberTextFieldState("eder.lopes@portugal.pt"),
+            modifier = Modifier.width(300.dp),
+            placeholder = "eder.lopes@portugal.pt",
+            title = "Email",
+            supportingText = "Please enter your email.",
+            enabled = false
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun AppTextFieldErrorPreview() {
+    AppTheme {
+        AppTextField(
+            state = rememberTextFieldState("eder.lopes@portugal.pt"),
+            modifier = Modifier.width(300.dp),
+            placeholder = "eder.lopes@portugal.pt",
+            title = "Email",
+            supportingText = "Please enter your email.",
+            isError = true
+        )
+    }
+}
